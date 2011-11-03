4e1a2e0521ac2785f67bdf83783d151b55dbfbb5
Jonathan Daugherty <jtd@galois.com>
2011-11-03 14:22:11 -0700

    Other misc changes, including turning off usually-annoying skeleton pair behavior

--- machines/prime.el
+++ machines/prime.el
@@ -2,7 +2,8 @@
 (setenv "PATH" (concat "/Users/cygnus/.bin:/Users/cygnus/.cabal/bin:/opt/local/bin:"
                        (getenv "PATH")))
 
-(set-default-font "Menlo:size=14")
+;;(set-default-font "Menlo:size=14")
+(set-default-font "Lucida Console:size=16")
 (tool-bar-mode)
 (menu-bar-mode)
 (my-color-theme-light)
@@ -10,8 +11,8 @@
 ;;(add-load-path "emacs_work")
 ;;(load-library "dotemacs")
 
-(set-variable 'skeleton-pair t)
-(global-set-key "(" 'skeleton-pair-insert-maybe)
-(global-set-key "[" 'skeleton-pair-insert-maybe)
-(global-set-key "{" 'skeleton-pair-insert-maybe)
-(global-set-key "\"" 'skeleton-pair-insert-maybe)
+;; (set-variable 'skeleton-pair t)
+;; (global-set-key "(" 'skeleton-pair-insert-maybe)
+;; (global-set-key "[" 'skeleton-pair-insert-maybe)
+;; (global-set-key "{" 'skeleton-pair-insert-maybe)
+;; (global-set-key "\"" 'skeleton-pair-insert-maybe)
